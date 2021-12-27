0781d6a4b9df2cdd8b098998f35bc334347c3f48
Ravi <mouryaravi@gmail.com>
2021-12-27 13:14:08 -0800

    send call/put alerts to diff channels in alpha

--- lib/beast/discord_bot.ex
+++ lib/beast/discord_bot.ex
@@ -10,6 +10,10 @@ defmodule Beast.DiscordBot do
     :os.system_time(:seconds)
   end
 
+  defp option_alert_alpha_text(ticker) do
+    "**#{ticker.readable_symbol}**  @   **#{ticker.price}**  in Range [#{ticker.beast_low} - #{ticker.beast_high}],   vwap: **#{ticker.vwap}**,    volume:   **#{ticker.volume}**"
+  end
+
   defp option_alert_text(ticker) do
     "**#{ticker.readable_symbol}**  @   **#{ticker.price}**  in  Beast Range [#{ticker.beast_low} - #{ticker.beast_high}]"
   end
@@ -21,12 +25,28 @@ defmodule Beast.DiscordBot do
     # Logger.info("****** SENT ALERT ****** #{inspect response} ")
   end
 
-  defp send_alert(ticker) do
-    urls = ["https://discord.com/api/webhooks/923079224141676584/3BQaQltxpU-JiYP0BdMDQ1ZTRj8dLBolnVLUO2yaVctYLL4jm--D8Cdt_EQ0_kbEsV6j",
-           "https://discordapp.com/api/webhooks/923078848638234685/_E0GVDJk5K8KLswW-Fy6aF2nMG0NBwhyJmZECri0CV3BuI2vw3LwcGH8AzK8UJ1TdPhj"]
+  def send_pt_alerts(ticker) do
+    urls = ["https://discordapp.com/api/webhooks/923078848638234685/_E0GVDJk5K8KLswW-Fy6aF2nMG0NBwhyJmZECri0CV3BuI2vw3LwcGH8AzK8UJ1TdPhj"]
     body = Poison.encode!(%{content: option_alert_text(ticker)})
     Enum.map(urls, fn url -> send_http_post(url, body) end)
     %{ticker | last_alerted_at: timestamp()}
+  end
+
+  def send_alpha_alerts(ticker) do
+    urls = %{puts: "https://discord.com/api/webhooks/925132918832136332/tQKcqA1UUsS9g59WSCr3kWygDHKCACwzrrYnbvbppfknECM6K8CZbXBztyOsUw1NMX9O",
+            calls: "https://discord.com/api/webhooks/925132209176838154/NTLj6VvysBYGmKvhL8IpfBe4yLvmQAAPh0xIQavjnGp8pQY6UfWE2YwNZKlTG5sp4ls5"}
+    body = Poison.encode!(%{content: option_alert_alpha_text(ticker)})
+    if ticker.type == "C" do
+      send_http_post(urls.calls, body)
+    else
+      send_http_post(urls.puts, body)
+    end
+    %{ticker | last_alerted_at: timestamp()}
+  end
+
+  defp send_alert(ticker) do
+    send_alpha_alerts(ticker)
+    send_pt_alerts(ticker)
   end
 
   def alert(ticker) do
